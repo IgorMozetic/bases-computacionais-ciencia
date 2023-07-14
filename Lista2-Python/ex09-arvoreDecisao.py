96ac37c52a21929c7ef3ce2601e278e31e7a41f1
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2023-07-14 19:41:42 -0300

    Lista 2 de exercícios em Python

--- Lista2-Python/ex09-arvoreDecisao.py
+++ Lista2-Python/ex09-arvoreDecisao.py
@@ -1,1 +1,21 @@
 # Exercício 2.9 - Árvore de Decisão para Jogar Tênis
+
+#enviando uma requisição ao cliente enviar o aspecto, a umidade e o vento
+aspecto = input()
+umidade = input()
+vento = input()
+
+#condição para testar se será possível jogar tenis
+if aspecto == "Sol":
+  if umidade == "Elevada":
+    print("N")
+  if umidade == "Normal":
+    print("S")
+elif aspecto == "Nuvens":
+  print("S")
+elif aspecto == "Chuva":
+  if vento == "Fraco":
+    print("S")
+  elif vento == "Forte":
+    print("N")
+    
