1822468a2040dc36bba7dc4d21e7bdfff91618b7
Igor Mozetic <igormozetic04@gmail.com>
2023-07-24 04:00:28 -0300

    Lista 3 de exercícios em Python

---
+++ Lista3-Python/ex06-contagio.py
@@ -0,0 +1,23 @@
+# Exercício 3.6 - Contágio
+
+#enviando uma requisição ao cliente enviar o número da população, da quantidade de pessoas infectadas no dia 0, a taxa de contágio e a porcentagem de imunidade
+populacao = int(input())
+qntDia0 = int(input())
+txContagio = float(input())
+imunidade = int(input())
+pessoasInfectadas = 0
+contagemPessoas = 0
+cont = 0
+
+# Laço de repetição para verificar se a quantidade de pessoas infectadas é superior a taxa de imunidade
+while (contagemPessoas/populacao*100) < imunidade:
+    if imunidade <= round((contagemPessoas*txContagio/populacao)*100):
+        print(cont)
+        break
+    if pessoasInfectadas != 0:
+        pessoasInfectadas = int((pessoasInfectadas*txContagio))
+        contagemPessoas += pessoasInfectadas
+    else:
+        pessoasInfectadas = qntDia0
+        contagemPessoas = qntDia0
+    cont+=1
